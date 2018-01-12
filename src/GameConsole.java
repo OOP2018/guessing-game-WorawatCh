611939b34de2549bbe9a5183296caea21a185cb4
Worawat Chueajedton <worawat.chu@ku.th>
2018-01-12 15:16:06 +0700

    Complete problem 4

--- src/GameConsole.java
+++ src/GameConsole.java
@@ -8,16 +8,17 @@ public class GameConsole {
 	/** play the game. */
 	public int play(NumberGame game) {
 		Scanner console = new Scanner(System.in);
-		
-		// describe the game
-		System.out.println( game.toString() );
-		
-		// This is just an example.
+		game.toString();
+	    System.out.println( game.toString() );
 		System.out.println( game.getMessage() );
-		System.out.print("Your answer? ");
-		int guess = console.nextInt();
-		boolean correct = game.guess(guess);
-		System.out.println( game.getMessage() );
+		boolean correct = false;
+		int guess = 0;
+		while(!correct){
+			System.out.print("Your answer? ");
+			 guess = console.nextInt();
+		    correct = game.guess(guess);
+			System.out.println( game.getMessage() );
+		}
 		return guess;
 	}
 	
---
+++ src/WorawatGame.java
@@ -0,0 +1,46 @@
+import java.util.Random; // for generating random numbers
+
+/**
+ * Game of guessing a secret number. -- write a description
+ * 
+ * @author Worawat Chueajedton
+ */
+public class WorawatGame extends NumberGame {
+	/* properties of a guessing game */
+	private int upperBound;
+	private int secret;
+
+	public WorawatGame(int upperBound) {
+		this.upperBound = upperBound;
+		this.secret = getRandomnumber(upperBound);
+		setMessage("I'm thinking of a number between 1 and 100");
+	}
+
+	public boolean guess(int number) {
+		if (number == secret) {
+			setMessage("Right! The secret number is " + secret);
+			return true;
+		} else if (number < secret) {
+			setMessage("Sorry, too small.");
+		} else if (number > secret) {
+			setMessage("Sorry, too large.");
+		}
+		return false;
+	}
+	
+	public int getRandomnumber(int upperBound){
+		long seed = System.nanoTime( );
+		Random rand = new Random( seed );
+		// get a random number between 0 and 9. Add 1 so the value is 1 - 10.
+		int value = rand.nextInt(10) + 1;
+		return value;
+	}
+
+	public int getUpperBound() {
+		return upperBound;
+	}
+
+	public String toString() {
+		return "Try to guess a number.";
+	}
+}
